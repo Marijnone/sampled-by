4e5ce574f0f4a5f636f8346e03d2de68430b32b1
Marijnone <marijn.moviat@hva.nl>
2019-08-02 17:43:45 +0200

    before react router

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 function ArtistCard (props) {
     // console.log(props);
    
-    //little hairy but dynamic system to make kaytranda white and tcq white could also take the length of the props maybe?
+    //little hairy but dynamic ternary system to make kaytranda white and tcq white could also take the length of the props maybe?
     return(
         <div className="artist-card">
         <h1 style={{color: props.Artist === "Kaytranada" || props.Artist === "A Tribe Called Quest" ? "white" : "black"}}>Sampled By  {props.Artist}</h1>
